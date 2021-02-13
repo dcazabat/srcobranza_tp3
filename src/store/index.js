e2ef5a56b7127a2e1bb2be1a1bf8dfee5dd22860
anibal317 <36904152+anibal317@users.noreply.github.com>
2021-02-13 20:22:50 -0300

    Se agregan funciones en store para traer y validar usuarios

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,8 @@ import {
   msgConexionFetchError,
   msgParseFetchError,
   msgUserFound,
-  msgUserNotFound
+  msgUserNotFound,
+  msgWrongPwd
 } from "../assets/js/urls";
 
 Vue.use(Vuex);
@@ -15,15 +16,26 @@ export default new Vuex.Store({
   state: {
     isLogged: false,
     personas: [],
-    users: []
+    users: {},
+    userId: "",
+    userPwd: "",
+    currentUser: {}
   },
   mutations: {
     setPersons(state, payload) {
       state.personas = payload.person;
       console.log(state.personas)
     },
-    setUsers(state, payload){
+    setUsers(state, payload) {
       state.users = payload.user
+    },
+    setCurrentuser(state, payload){
+      if(payload.currentUser.pwd === userPwd){
+        state.currentUser = payload.currentUser;
+        state.isLogged = true;
+      }else{
+        consoele.log(msgWrongPwd)
+      }
     }
   },
   actions: {
@@ -34,11 +46,18 @@ export default new Vuex.Store({
         .then((person) => commit("setPersons", { person }))
         .catch(error => console.log(msgParseFetchError, error));
     },
-    getAllUsers({ commit }){
+    getAllUsers({ commit }) {
       fetch(userUrl)
         .then(res => res.json())
         .catch(err => console.log(msgConexionFetchError, err))
         .then((user) => commit("setUsers", { user }))
+        .catch(error => console.log(msgParseFetchError, error));
+    },
+    getUserById({ commit },userId) {
+      fetch(baseUrl+"/users/"+userId+".json")
+        .then(response => response.json())
+        .catch(err => console.log(msgConexionFetchError, err))
+        .then((currentUser) => commit("setCurrentuser",{ currentUser }))
         .catch(error => console.log(msgParseFetchError, error));
     }
   },
@@ -49,10 +68,10 @@ export default new Vuex.Store({
     getusers(state) {
       return state.users
     },
-    findUser(state, userId){
-      if(state.users === userId){
+    findUser(state, userId) {
+      if (state.users.find(user => Object.key(user) === userId)) {
         console.log(msgUserFound)
-      }else{
+      } else {
         console.log(msgUserNotFound)
       }
     }
