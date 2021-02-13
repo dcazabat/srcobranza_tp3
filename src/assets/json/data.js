0b51581e5694cdc92177d262a7b0be2f6c0d8c05
anibal317 <36904152+anibal317@users.noreply.github.com>
2021-02-13 17:28:43 -0300

    Configuración del store y variables globales

--- src/assets/json/data.js
+++ src/assets/json/data.js
@@ -1,72 +1,72 @@
-let personas = [
+personas = [
   {
-      id:1,
-      nombre:"Antonio Neruda",
-      pais:"Mexico",
-      edad:26,
-      ocupacion:"Estudiante"
+      "id":1,
+      "nombre":"Antonio Neruda",
+      "pais":"Mexico",
+      "edad":26,
+      "ocupacion":"Estudiante"
   },
   {
-      id:2,
-      nombre:"Juan Ponce de Leon",
-      pais:"Ecuador",
-      edad:30,
-      ocupacion:"Ingeniero en sistemas"
+      "id":2,
+      "nombre":"Juan Ponce de Leon",
+      "pais":"Ecuador",
+      "edad":30,
+      "ocupacion":"Ingeniero en sistemas"
   },
   {
-      id:3,
-      nombre:"Ximena Xanders",
-      pais:"Argentina",
-      edad:20,
-      ocupacion:"Disenadora grafica"
+      "id":3,
+      "nombre":"Ximena Xanders",
+      "pais":"Argentina",
+      "edad":20,
+      "ocupacion":"Disenadora grafica"
   },
   {
-      id:4,
-      nombre:"Alberto Contreras",
-      pais:"Chile",
-      edad:29,
-      ocupacion:"Gerente de operaciones"
+      "id":4,
+      "nombre":"Alberto Contreras",
+      "pais":"Chile",
+      "edad":29,
+      "ocupacion":"Gerente de operaciones"
   },
   {
-      id:5,
-      nombre:"Marcela Polonio",
-      pais:"Mexico",
-      edad:24,
-      ocupacion:"Consultora"
+      "id":5,
+      "nombre":"Marcela Polonio",
+      "pais":"Mexico",
+      "edad":24,
+      "ocupacion":"Consultora"
   },
   {
-      id:6,
-      nombre:"Carmen Di Geronimo",
-      pais:"Chile",
-      edad:27,
-      ocupacion:"Abogada"
+      "id":6,
+      "nombre":"Carmen Di Geronimo",
+      "pais":"Chile",
+      "edad":27,
+      "ocupacion":"Abogada"
   },
   {
-      id:7,
-      nombre:"Salvador Padilla",
-      pais:"Argentina",
-      edad:49,
-      ocupacion:"Analista de datos"
+      "id":7,
+      "nombre":"Salvador Padilla",
+      "pais":"Argentina",
+      "edad":49,
+      "ocupacion":"Analista de datos"
   },
   {
-      id:8,
-      nombre:"Marcelo Williams",
-      pais:"Mexico",
-      edad:39,
-      ocupacion:"Desarrollador de software"
+      "id":8,
+      "nombre":"Marcelo Williams",
+      "pais":"Mexico",
+      "edad":39,
+      "ocupacion":"Desarrollador de software"
   },
   {
-      id:9,
-      nombre:"Georgina Georgalos",
-      pais:"Ecuador",
-      edad:31,
-      ocupacion:"Recepcionista"
+      "id":9,
+      "nombre":"Georgina Georgalos",
+      "pais":"Ecuador",
+      "edad":31,
+      "ocupacion":"Recepcionista"
   },
   {
-      id:10,
-      nombre:"Gilberto Gil",
-      pais:"Chile",
-      edad:23,
-      ocupacion:"Calidad"
+      "id":10,
+      "nombre":"Gilberto Gil",
+      "pais":"Chile",
+      "edad":23,
+      "ocupacion":"Calidad"
       }
 ];
