971336bb0951eeaad9a8cc041a9f441a63c82453
Daniel Cazabat <dacazabat@gmail.com>
2021-02-13 17:29:30 -0300

    Componente Nav y Corruosel, View Home

--- src/components/Carrousel.vue
+++ src/components/Carrousel.vue
@@ -1,10 +1,107 @@
 <template>
-  <div></div>
+  <div class="row justify-content-center">
+    <div id="srcarrousel" class="carousel slide w-75" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#srcarrousel" data-slide-to="0" class="active"></li>
+        <li data-target="#srcarrousel" data-slide-to="1"></li>
+        <li data-target="#srcarrousel" data-slide-to="2"></li>
+        <li data-target="#srcarrousel" data-slide-to="3"></li>
+        <li data-target="#srcarrousel" data-slide-to="4"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img
+            src="../assets/images/cob01.jpg"
+            class="d-block w-100"
+            alt="..."
+          />
+          <div class="carousel-caption d-none d-md-block">
+            <h3 class="text-ligth">Convicción</h3>
+            <p class="text-ligth">
+              La fuerza que nos caracteriza y nos impulsa.
+            </p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img
+            src="../assets/images/cob02.jpg"
+            class="d-block w-100"
+            alt="..."
+          />
+          <div class="carousel-caption d-none d-md-block">
+            <h3 class="text-dark">Simpleza</h3>
+            <p class="text-dark">
+              Nuestros servicios son sencillos, para Uds. y su Cliente.
+            </p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img
+            src="../assets/images/cob03.jpg"
+            class="d-block w-100"
+            alt="..."
+          />
+          <div class="carousel-caption d-none d-md-block">
+            <h3 class="text-dark">Costos</h3>
+            <p class="text-dark">
+              El valor exacto de nuestros servicio y el costo justo para su
+              Empresa.
+            </p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img
+            src="../assets/images/cob04.jpg"
+            class="d-block w-100"
+            alt="..."
+          />
+          <div class="carousel-caption d-none d-md-block">
+            <h3 class="text-dark">Medios de Pago</h3>
+            <p class="text-dark">
+              La Multiplicidad de opciones para realizar sus cobros nos
+              caracteriza.
+            </p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img
+            src="../assets/images/cob05.jpg"
+            class="d-block w-100"
+            alt="..."
+          />
+          <div class="carousel-caption d-none d-md-block">
+            <h3 class="text-ligth">Casos de Exito</h3>
+            <p class="text-ligth">
+              Una de las Entedidas que confian en Nosotros.
+            </p>
+          </div>
+        </div>
+      </div>
+      <a
+        class="carousel-control-prev"
+        href="#srcarrousel"
+        role="button"
+        data-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Anterior</span>
+      </a>
+      <a
+        class="carousel-control-next"
+        href="#srcarrousel"
+        role="button"
+        data-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Siguiente</span>
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped>
+<style>
 </style>
