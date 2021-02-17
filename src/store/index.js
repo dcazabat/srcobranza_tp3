c42e693fe3560d4b07200d021a2165fdb72efb87
Jorge Sardon <anibal317@gmail.com>
2021-02-17 18:42:46 -0300

    Se crean los actions para cobradores y se crea nueva url

--- src/store/index.js
+++ src/store/index.js
@@ -20,6 +20,7 @@ export default new Vuex.Store({
     isLogged: false,
     isRegister: false,
     personas: [],
+    cobradores: [],
     users: {},
     userId: "admin",
     userPwd: "admin*",
@@ -29,6 +30,10 @@ export default new Vuex.Store({
     setPersons(state, payload) {
       state.personas = payload.person;
       console.log(state.personas)
+    },
+    setCobrador(state, payload) {
+      state.cobradores = payload.cobrador;
+      console.log(state.cobradores)
     },
     setUsers(state, payload) {
       state.users = payload.user
@@ -46,9 +51,7 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    getAllPerons({
-      commit
-    }) {
+    getAllPerons({ commit }) {
       fetch(personsUrl)
         .then(res => res.json()).catch(err => console.log(msgConexionFetchError, err))
         .then((person) => commit("setPersons", {
@@ -56,9 +59,7 @@ export default new Vuex.Store({
         }))
         .catch(error => console.log(msgParseFetchError, error));
     },
-    getAllUsers({
-      commit
-    }) {
+    getAllUsers({ commit }) {
       fetch(userUrl)
         .then(res => res.json())
         .catch(err => console.log(msgConexionFetchError, err))
@@ -67,8 +68,16 @@ export default new Vuex.Store({
         }))
         .catch(error => console.log(msgParseFetchError, error));
     },
-    patchNewUser(user_id, data) {
-      let newUser = `{"${JSON.parse(user_id)}": ${JSON.stringify(data)}}`;
+    getAllCobradores({ commit }) {
+      fetch(cobradoresUrl)
+        .then(res => res.json()).catch(err => console.log(msgConexionFetchError, err))
+        .then((cobrador) => commit("setPersons", {
+          cobrador
+        }))
+        .catch(error => console.log(msgParseFetchError, error));
+    },
+    patchNewUser(context, payload) {
+      let newUser = `{"${JSON.parse(paylaod.user_id)}": ${JSON.stringify(payload.data)}}`;
 
       let myHeaders = new Headers();
       myHeaders.append("Content-Type", "application/json");
@@ -87,19 +96,15 @@ export default new Vuex.Store({
         .catch(error => console.log('error', error));
 
     },
-    postPerson(state) {
+    postPerson(context, payload) {
+      let newPerson = JSON.stringify(payload.data);
+
       let myHeaders = new Headers();
       myHeaders.append("Content-Type", "application/json");
 
-      var raw = JSON.stringify({
-        "id": 10,
-        "nombre": "Gilberto Gil",
-        "pais": "Chile",
-        "edad": 23,
-        "ocupacion": "Calidad"
-      });
+      let raw = newPerson;
 
-      var requestOptions = {
+      let requestOptions = {
         method: 'POST',
         headers: myHeaders,
         body: raw,
@@ -107,7 +112,28 @@ export default new Vuex.Store({
       };
 
       fetch("https://sr-cobranza-default-rtdb.firebaseio.com/personas.json", requestOptions)
-        .then(response => response.text())
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+      console.log(state);
+    },
+    postcobrador(context, payload) {
+      let newCobrador = JSON.stringify(payload.data);
+
+      let myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      let raw = newCobrador;
+
+      let requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+
+      fetch(cobradoresUrl, requestOptions)
+        .then(response => response.json())
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
       console.log(state);
@@ -116,6 +142,9 @@ export default new Vuex.Store({
   getters: {
     getPersons(state) {
       return state.personas
+    },
+    getCobradores(state) {
+      return state.cobradores
     },
     getusers(state) {
       return state.users
