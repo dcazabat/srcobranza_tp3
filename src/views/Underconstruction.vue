d5805278f65e00d180af775412d72c8e25c6375a
Jorge Sardon <anibal317@gmail.com>
2021-02-23 18:17:26 -0300

    Se estila under construction

--- src/views/Underconstruction.vue
+++ src/views/Underconstruction.vue
@@ -2,7 +2,10 @@
   <div id="B404" class="error-container mb-6">
     <h3>Opps !!!, a donde vamos !!!</h3>
     <hr />
-    <h1>UNDERCONSTRACTION</h1>
+    <div class="container">
+      <div class="display-1 text-warning">UNDER</div>
+      <div class="display-3 text-warning">CONSTRUCTION</div>
+    </div>
     <p>Sección en constrcción. Vuelve pronto!</p>
     <hr />
     <h3>Asi no llegamos a ningun lado</h3>
@@ -13,21 +16,19 @@
 export default {};
 </script>
 
-<style >
-.B404 {
-  background-color: #2d72d9;
-  color: #fff;
+<style>
+
+#B404 {
+  color: white;
   -moz-font-smoothing: antialiased;
   -webkit-font-smoothing: antialiased;
+  background-image: url("../assets/images/under.jpg");
+  background-repeat: no-repeat;
+
 }
 
 .error-container {
   text-align: center;
-}
-
-.error-container h1 {
   margin: 0;
-  font-size: 160px;
-  font-weight: 300;
 }
 </style>
